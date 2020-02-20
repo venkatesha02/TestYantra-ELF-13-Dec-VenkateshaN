61becfe186f2cb63783adc61f7f91dfb1a0d0901
venkatesh <venkatesh000venki@gmail.com>
2020-02-21 00:07:45 +0530

    react + java

--- TestYantra-React/JavaScript-Assessment/Assessment 2/OMDB/omdb.js
+++ TestYantra-React/JavaScript-Assessment/Assessment 2/OMDB/omdb.js
@@ -9,26 +9,25 @@ function search() {
         .then(response => response.json())
         .then(json => {
 
-
-            const { Title, Year, imdbID, Type, Poster } = json
+            //const { Title, Year, imdbID, Type, Poster } = json
             for (let i = 0; i < json.Search.length; i++) {
-                console.log(json.Search[i])
-
+                // console.log(json.Search[i])
+                
                 let div1 = document.createElement('div')
                 let div2 = document.createElement('div')
                 let div3 = document.createElement('div')
                 let img = document.createElement('img')
-                let h5 = document. //createElement('h5')
+                let h5 = document.createElement('h5')
                 let p1 = document.createElement('p')
                 let p2 = document.createElement('p')
                 let p3 = document.createElement('p')
-
+                
                 img.src = json.Search[i].Poster
                 h5.textContent = 'Title : ' + json.Search[i].Title
                 p1.textContent = "Year : " + json.Search[i].Year
                 p2.textContent = 'IMDB ID : ' + json.Search[i].imdbID
                 p3.textContent = 'Type: ' + json.Search[i].Type
-
+                
                 div1.classList = 'col-md-3 float-left'
                 div2.classList = 'card'
                 div3.classList = 'card-body'
@@ -36,7 +35,8 @@ function search() {
                 p1.classList = 'card-text'
                 p2.classList = 'card-text'
                 p3.classList = 'card-text'
-
+                
+                div1.innerHTML=''
                 div1.appendChild(div2)
                 div2.appendChild(div3)
                 div3.appendChild(img)
